4281a3fc48cd1fb829596767d6c2b9e160cc2220
Danielaghughu <danielaghughu131@gmail.com>
2024-09-04 00:25:59 -0400

    classjv

---
+++ assignment/ass.js
@@ -0,0 +1,68 @@
+let firstName = 'Daniel';
+let lastName = 'Aghughu';
+let age = '20';
+let favouriteColor = 'Black'
+let favouriteAnimal = 'Dog'
+// let currentYear = '2024'
+// let  birthYear = '2004'
+ 
+
+
+let fullName = `Hello, Mr ${firstName} ${lastName}. How are you doing today Mr Daniel Aghughu`;
+console.log(fullName)
+
+let firstNumber = parseInt(prompt('Enter your first number'));
+let secondNumber = parseInt(prompt('Enter your second number'));
+console.log(`${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}`)
+
+
+let greetingmessage = `Hello, ${firstName} ${lastName} i am ${age} years old.`;
+console.log(greetingmessage);
+
+let combinemessage = `My favourite color is, ${favouriteColor} and my favouriteAnimal is ${favouriteAnimal}.`;
+console.log(combinemessage);
+
+// let currentYear = ('2024')
+// let birthYear = ('2004')
+// let print = currentYear - birthYear;
+// console.log(`hello,` ${firstName} )
+
+
+let hometown = 'Edo state';
+let favouriteFood = 'Eba and Egusi soup';
+let home = `i am from ${hometown} and my favouriteFood is ${favouriteFood}.`;
+console.log(home)
+
+let bookTitle = 'hiding in the shadows';
+let autor = 'Linda Howard';
+let book = `i recently read ${bookTitle} by ${autor}.`;
+console.log(book)
+
+
+let cityName = 'ibadan';
+let visitYear = '2023';
+let city = `i visted ${cityName} in the year ${visitYear}.`;
+console.log(city)
+
+
+let favouriteMovie = 'BraveHeart'
+let releaseYear = '1995'
+let movie =`my favourite Movie is ${favouriteMovie} and it was release on july 1st ${releaseYear}.`;
+console.log(movie)
+
+
+let favouriteFruit = 'Banana'
+let numberofTime = '3times'
+let fruit = `my favourite furit is ${favouriteFruit} and i eat it ${numberofTime} everyweek.`;
+console.log(fruit)
+
+let favouritesubject = 'English'
+let grade = 'B2'
+let subject = `my favourite subject in school is ${favouritesubject} and my grade is ${grade}.`;
+console.log(subject)
+
+
+let favouriteSport = 'Basketball'
+let season = 'April'
+let sport = `my favourite sport is ${favouriteSport} and it is always played on every ${season} until the next year.`;
+console.log(sport)
