70c9d45a5ca950f08adc62584ff12b9ca3f249a6
Digital-Fortress-Institute <platform@gigitalfortressltd.com>
2024-09-04 13:08:03 +0100

    class

--- tunde.js
+++ tunde.js
@@ -7,11 +7,11 @@
 
 // syntax used in setting variables in javascripts;
 
-// var x = 10;
+var x = 10;
 
-// var x = 30;
-// console.log(x)
-// ++x
+var x = 30;
+console.log(x)
+++x
 
 // let mistura = 50;
 // ++mistura
@@ -40,7 +40,61 @@
 // let firtName = prompt("Enter your first name");
 // let lastName = prompt('Enter your last name');
 // console.log(`${firtName} ${lastName}`);
-let firstNumber = parseInt(prompt('Enter your first number'));
-let secondNumber = parseInt(prompt('Enter your second number'));
-console.log(`${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}`)
+// let firstNumber = parseInt(prompt('Enter your first number'));
+// let secondNumber = parseInt(prompt('Enter your second number'));
+// console.log(`${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}`)
+
+
+// let firstName = prompt('Enter your first name');
+// let lastNAme = prompt('Enter your last name');
+
+// console.log(`good morning ${firstName}, ${lastNAme}`);
+
+
+// let num1 = parseInt(prompt('Enter your first number'));
+// let num2 = parseInt(prompt('Enter your second number'))
+// console.log(`${num1} + ${num2} = ${num1 + num2}`);
+
+// let firstName = prompt('Enter your first name');
+// let lastNAme = prompt('Enter your last name');
+// let age = prompt('Enter your age')
+
+// console.log(`good morning ${firstName} ${lastNAme} I am ${age} years olod`);
+
+
+// let animal = prompt('Enter your favorite animal');
+// let myColor = prompt('Enter your favourite color');
+// console.log(` My favourite animal is ${animal} and my best is ${myColor}`)
+
+// let yourName = prompt('Enter your first name');
+// let birthyear = prompt('Enter your birth year');
+// let yourAge = 2024 -  birthyear
+
+// console.log(` My name is ${yourName}, my birth year is ${birthyear} and which makes my age ${yourAge}`);
+
+
+// let myFood = prompt('Enter your best food');
+// let homeTown = prompt('Enter your home town');
+// console.log('My favourite food is'+ ' '+ myFood+ ' '+ ' '+ ' ' +'my home tow is'+ ' '+ homeTown);
+// Data Types
+// Strings: this data type is alphanumeric data. 
+
+let address = '107, Herbert Macauly Way, Off \'University\' Road, Akoka Yaba, Lagos State';
+console.log(address)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
